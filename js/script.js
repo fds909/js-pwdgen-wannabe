6478be6610b833d18aa3a083f2014f6116a77f9b
Francesco De Sìmini <francescod995@gmail.com>
2022-03-08 14:47:38 +0100

    completo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // Chiedi all’utente il suo nome,
 // poi chiedi il suo cognome,
 // poi chiedi il suo colore preferito
-
 // Infine scrivi sulla pagina: nomecognomecolorepreferito21
 
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
 
+let password = nome + cognome + colorePreferito + "21";
+
+document.getElementById('password').innerHTML = password;
